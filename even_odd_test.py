a39ec513ecbf0874b06a32baa5d761be4c45ef24
Nazriel-commits <nazrielwork@gmail.com>
2025-05-13 11:05:53 +0800

    Update even_odd_test.py
    added new section that tests how the updated code handles certain float inputs

--- even_odd_test.py
+++ even_odd_test.py
@@ -5,9 +5,21 @@ def test_is_even_or_odd():
     # Test even/odd
     assert is_even_or_odd(2) == "even"
     assert is_even_or_odd(3) == "odd"
+    
+    # Test float handling - Flaots that represent whole numbers should be accepted
+    assert is_even_or_odd(4.0) == "even"
+    assert is_even_or_odd(12.000000) == "even"
+
+    #Test non-integer float should raise an exception
+    with pytest.raises(TypeError):
+        is_even_or_odd(4.3)
+    with pytest.raises(TypeError):
+        is_even_or_odd(12.33453445)
+        
     # Test edge cases (zero, negative)
     assert is_even_or_odd(0) == "even"
     assert is_even_or_odd(-1) == "odd"
-    # Test exception
+    
+    # Test exception for non-numberic inputs (string)
     with pytest.raises(TypeError):
-        is_even_or_odd("not a number")
+        is_even_or_odd("not a number") 
